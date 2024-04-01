64daef22e9493f88084742b9bc0f9686b8058a56
shubhankarsingh030 <113350090+shubhankarsingh030@users.noreply.github.com>
2024-04-01 21:28:48 +0530

    Tinder Clone

---
+++ script.js
@@ -0,0 +1,231 @@
+let users =[
+    {
+        profilePicture: "https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPicture:"https://images.unsplash.com/photo-1611601679655-7c8bc197f0c6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D",
+        pendingMessage: 2,
+        location: "Moscow,Russia",
+        name:"Lucia",
+        age:23,
+        intrest:[{
+            icon:`<i class="ri-headphone-fill"></i>`,
+            intrest:"Music",
+        },{
+            icon:`<i class="ri-ball-pen-fill"></i>`,
+            intrest:`Writing`,
+        },{
+            icon:`<i class="ri-car-fill"></i>`,
+            intrest:`Traveling`,
+        }],
+        bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit.a nulla quidem odio sed, magni officiis dolorem reprehenderit est.",
+        isFriend:null
+    },
+    {
+        profilePicture: "https://images.unsplash.com/photo-1616091093714-c64882e9ab55?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPicture:"https://images.unsplash.com/photo-1616091093623-cf4b7bbd2e18?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D",
+        pendingMessage: 4,
+        location: "Saints Peterburg",
+        name:"Shirley",
+        age:25,
+        intrest:[{
+            icon:`<i class="ri-headphone-fill"></i>`,
+            intrest:"Music",
+        },{
+            icon:`<i class="ri-ball-pen-fill"></i>`,
+            intrest:`Writing`,
+        },{
+            icon:`<i class="ri-car-fill"></i>`,
+            intrest:`Traveling`,
+        }],        bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit.a nulla quidem odio sed, magni officiis dolorem reprehenderit est.",
+        isFriend:null
+    },
+    {
+        profilePicture: "https://images.unsplash.com/photo-1625376212522-78872a686380?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        displayPicture:"https://images.unsplash.com/photo-1625325364969-958291c458bc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D",
+        pendingMessage: 1,
+        location: "London, UK",
+        name:"Lauria",
+        age:26,
+        intrest:[{
+            icon:`<i class="ri-headphone-fill"></i>`,
+            intrest:"Music",
+        },{
+            icon:`<i class="ri-ball-pen-fill"></i>`,
+            intrest:`Writing`,
+        },{
+            icon:`<i class="ri-car-fill"></i>`,
+            intrest:`Traveling`,
+        }],        bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit.a nulla quidem odio sed, magni officiis dolorem reprehenderit est.",
+        isFriend:null
+    },
+    {
+        profilePicture: "https://images.unsplash.com/photo-1636882336669-962bc700280b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D",
+        displayPicture:"https://images.unsplash.com/photo-1636882337136-847953c51c53?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        pendingMessage: 3,
+        location: "Manchester, UK",
+        name:"Harnha",
+        age:21,
+        intrest:[{
+            icon:`<i class="ri-headphone-fill"></i>`,
+            intrest:"Music",
+        },{
+            icon:`<i class="ri-ball-pen-fill"></i>`,
+            intrest:`Writing`,
+        },{
+            icon:`<i class="ri-car-fill"></i>`,
+            intrest:`Traveling`,
+        }],        bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit.a nulla quidem odio sed, magni officiis dolorem reprehenderit est.",
+        isFriend:null
+    },
+    {
+        profilePicture: "https://images.unsplash.com/photo-1615805159543-043d26ec1080?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D",
+        displayPicture:"https://images.unsplash.com/photo-1615805159563-afb93520d5e7?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        pendingMessage: 4,
+        location: "Los Angeles, USA",
+        name:"Monica",
+        age:23,
+        intrest:[{
+            icon:`<i class="ri-headphone-fill"></i>`,
+            intrest:"Music",
+        },{
+            icon:`<i class="ri-ball-pen-fill"></i>`,
+            intrest:`Writing`,
+        },{
+            icon:`<i class="ri-car-fill"></i>`,
+            intrest:`Traveling`,
+        }],        bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit.a nulla quidem odio sed, magni officiis dolorem reprehenderit est.",
+        isFriend:null
+    },
+    {
+        profilePicture: "https://images.unsplash.com/photo-1616790876844-97c0c6057364?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D",
+        displayPicture:"https://images.unsplash.com/photo-1616788034199-bd16c201eadf?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        pendingMessage: 3,
+        location: "Los Angeles, USA",
+        name:"Marnita",
+        age:24,
+        intrest:[{
+            icon:`<i class="ri-headphone-fill"></i>`,
+            intrest:"Music",
+        },{
+            icon:`<i class="ri-ball-pen-fill"></i>`,
+            intrest:`Writing`,
+        },{
+            icon:`<i class="ri-car-fill"></i>`,
+            intrest:`Traveling`,
+        }],        bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit.a nulla quidem odio sed, magni officiis dolorem reprehenderit est.",
+        isFriend:null
+    },
+]
+
+function select(elem){
+    return document.querySelector(elem);
+}
+
+let curr = 0;
+let isAnimating = false;
+
+function setData(index){
+    select(".profileimg img").src = users[index].profilePicture;
+    select(".badge h5").textContent = users[index].pendingMessage;
+    select(".location h3").textContent = users[index].location;
+    select(".name h1:nth-child(1)").textContent = users[index].name;
+    select(".name h1:nth-child(2)").textContent = users[index].age;
+
+    let clutter = "";
+    users[index].intrest.forEach(function(intrest){
+        clutter += `<div class="tag flex items-center bg-white/30 px-3 rounded-full py-1 gap-3">
+        ${intrest.icon}
+        <h3 class="text-sm tracking-tighter ">${intrest.intrest}</h3>
+    </div>`
+    })
+    select(".tags").innerHTML = clutter;
+
+    select(".bio p").textContent = users[index].bio
+}
+
+(function setInitial() {
+    select(".maincard img").src = users[curr].displayPicture;
+    select(".incomingcard img").src = users[curr + 1].displayPicture;
+
+    setData(curr);
+
+    curr = 2
+})()
+
+function imageChange(){
+    if(!isAnimating){
+        isAnimating = true;
+        let tl = gsap.timeline({
+                onComplete: function(){
+                    isAnimating = false;
+                    let main = select(".maincard");
+                    let incoming =select(".incomingcard");
+
+                    incoming.classList.remove("z-[2]");
+                    incoming.classList.add("z-[3]");
+                    incoming.classList.remove("incomingcard");
+
+                    main.classList.remove("z-[3]");
+                    main.classList.add("z-[2]");
+                    gsap.set(main,{
+                        scale: 1,
+                        opacity:1
+                    })
+                    if (curr === users.length) curr =0;
+                    select(".maincard img").src = users[curr].displayPicture;
+                    curr++;
+                    main.classList.remove("maincard");
+                    incoming.classList.add("maincard");
+                    main.classList.add("incomingcard")
+                }
+            });
+            tl.to(".maincard", {
+                scale:1.1,
+                opacity:0,
+                ease: Circ,
+                duration:.9
+            },"a")
+            tl.from(".incomingcard", {
+                scale:.9,
+                opacity:0,
+                ease: Circ,
+                duration:1.1
+            },"a")
+    }
+}
+    
+
+let deny = select(".deny");
+let accept = select(".accept");
+
+deny.addEventListener("click", function(){
+    imageChange();
+    setData(curr-1);
+    gsap.from(".details .elements",{
+        y:"100%",
+        opacity:0,
+        stagger: .07,
+        ease: Circ,
+        duration:1
+    });
+});
+accept.addEventListener("click", function(){
+    imageChange();
+    setData(curr-1);
+    gsap.from(".details .elements",{
+        y:"100%",
+        opacity:0,
+        stagger: .07,
+        ease: Circ,
+        duration:1
+    });
+});
+
+(function containerCreator(){
+    document.querySelectorAll(".elements")
+    .forEach(function(elements){
+        let div = document.createElement("div");
+        div.classList.add(`${elements.classList[1]}container`, "overflow-hidden")
+        div.appendChild(elements)
+        select(".details").appendChild(div);
+    })
+})();
